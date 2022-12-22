18e0bbea26f608172db4f46ccc9af14a57bbad5d
Simon Saberian <simon.saberian@gmail.com>
2022-12-23 00:21:19 +0100

    add foreground location service

--- app/src/main/java/com/example/runningwithbill/location/LocationService.kt
+++ app/src/main/java/com/example/runningwithbill/location/LocationService.kt
@@ -1,6 +1,88 @@
 package com.example.runningwithbill.location
 
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.runningwithbill.R
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsResult
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+const val FIVE_SEC : Long = 5*1000
 
 class LocationService : Service() {
+
+    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private lateinit var locationClient: LocationClient
+    private val notificationId = 1
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        locationClient = LocationClient(
+            applicationContext,
+            LocationServices.getFusedLocationProviderClient(applicationContext)
+        )
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when(intent?.action) {
+            ACTION_START -> start()
+            ACTION_STOP -> stop()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun start() {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Tracking location")
+            .setContentText("Location: null")
+            .setSmallIcon(R.drawable.ic_baseline_pets_24)
+            .setOngoing(true)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        locationClient
+            .getLocationUpdates(FIVE_SEC)
+            .catch { e -> e.printStackTrace() }
+            .onEach { location ->
+                val lat = location.latitude
+                val long = location.longitude
+                val updatedNotification = notification.setContentText(
+                    "Location: ($lat, $long)"
+                )
+                notificationManager.notify(notificationId, updatedNotification.build())
+            }
+            .launchIn(serviceScope)
+
+        startForeground(notificationId, notification.build())
+    }
+
+    private fun stop() {
+        stopForeground(true)
+        stopSelf()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+
+    companion object {
+        const val ACTION_START = "ACTION_START"
+        const val ACTION_STOP = "ACTION_STOP"
+        const val channelId = "location"
+    }
 }
